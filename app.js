1ac8f0086cfe8f90014d9a07344617a894af0b93
ThienTk <thanhthientk69@gmail.com>
2017-02-26 10:54:45 +0700

    second

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+console.log('App running!');
+
 app.use('/', routes);
 app.use('/users', users);
 
